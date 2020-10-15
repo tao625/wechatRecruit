55a9f20286ccb2b87b4151de8387b1e5f1878d2a
dan <1113965383@qq.com>
2020-10-15 15:22:21 +0800

    feat: Hide djcelery

--- apps/recruit/adminx.py
+++ apps/recruit/adminx.py
@@ -130,11 +130,11 @@ class UploadFileAdmin(object):
 
 
 # 定时任务表
-xadmin.site.register(IntervalSchedule)  # 存储循环任务设置的时间
-xadmin.site.register(CrontabSchedule)  # 存储定时任务设置的时间
-xadmin.site.register(PeriodicTask)  # 存储任务
-xadmin.site.register(TaskState)  # 存储任务执行状态
-xadmin.site.register(WorkerState)  # 存储执行任务的worker
+# xadmin.site.register(IntervalSchedule)  # 存储循环任务设置的时间
+# xadmin.site.register(CrontabSchedule)  # 存储定时任务设置的时间
+# xadmin.site.register(PeriodicTask)  # 存储任务
+# xadmin.site.register(TaskState)  # 存储任务执行状态
+# xadmin.site.register(WorkerState)  # 存储执行任务的worker
 
 # 常量
 xadmin.site.register(Constance, ConstanceAdmin)
